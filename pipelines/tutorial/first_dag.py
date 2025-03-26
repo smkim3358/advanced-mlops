5264452a60d2da1ed75375f38ccabead05732fba
smkim3358 <41354790+smkim3358@users.noreply.github.com>
2025-03-26 16:36:33 +0900

    tutorial code and training code write

--- pipelines/tutorial/first_dag.py
+++ pipelines/tutorial/first_dag.py
@@ -10,22 +10,17 @@ from utils.callbacks import failure_callback, success_callback
 local_timezone = pendulum.timezone("Asia/Seoul")
 
 with DAG(
-    # TODO: "simple_dag"이라는 이름의 DAG 설정
-    dag_id="",
-    # TODO: default_args에는 다음 내용이 들어감
-    # TODO: "user" 사용자가 소유한 DAG / 본인의 이메일 / 실패 및 재시도 시 이메일 알림 여부
-    # TODO: 재시도 1회 / 재시도 간격 5분
-    # TODO: 실패 시 callback (failure_callback) / 성공 시 callback (success_callback)
-    default_args={
-        "owner": "",
-        "depends_on_past": None,
-        "email": "",
-        "email_on_failure": None,
-        "email_on_retry": None,
-        "retries": None,
-        "retry_delay": None,
-        "on_failure_callback": None,
-        "on_success_callback": None,
+    dag_id="simple_dag", # "simple_dag"이라는 이름의 DAG 설정
+    default_args={ # default_args에는 다음 내용이 들어감
+        "owner": "user", # "user" 사용자가 소유한 DAG
+        "depends_on_past": False,
+        "email": "temp@example.com", # 본인의 이메일
+        "email_on_failure": False, # 실패 및 재시도 시 이메일 알림 여부 x
+        "email_on_retry": False, # 실패 및 재시도 시 이메일 알림 여부 x
+        "retries": 1, # 재시도 1회
+        "retry_delay": timedelta(minutes=5), # 재시도 간격 5분
+        "on_failure_callback": failure_callback, # 실패 시 callback
+        "on_success_callback": success_callback, # 성공 시 callback
     },
     description="Simple airflow dag",
     schedule="0 15 * * *",
@@ -35,12 +30,13 @@ with DAG(
 ) as dag:
     task1 = BashOperator(
         task_id="print_date",
-        # TODO: 현재 시간을 출력하는 bash_command 입력
+        bash_command="date", # 현재 시간을 출력하는 bash_command 입력
     )
     task2 = BashOperator(
         task_id="sleep",
         depends_on_past=False,
-        # TODO: 5초 sleep하는 bash_command를 입력하고 3회 재시도하도록 설정
+        bash_command="sleep 5", # 5초 sleep하는 bash_command
+        retries=3, # 3회 재시도하도록 설정
     )
 
     loop_command = dedent(
